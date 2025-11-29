ab9e9cf71bd000b2a23393827ade9118aa7cc942
Ashton Coates <a.coates.8537@gmail.com>
2025-11-29 16:51:18 -0600

    finalized project structure

--- king_tensor/screen.py
+++ king_tensor/screen.py
@@ -1,9 +1,14 @@
 import cv2
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from dataclasses import dataclass
 import yaml
 import pathlib
+
+matplotlib.use('TkAgg')
+
+PACKAGE_DIR = pathlib.Path(__file__).resolve().parents[0]
 
 @dataclass
 class ROI:
@@ -28,17 +33,44 @@ class Screen:
         self.img = img
 
         with open(config, 'r') as file:
-            try:
-                data = yaml.safe_load(file)
-                print(data)
-            except yaml.YAMLError as exc:
-                print(exc)
+            self.data = yaml.safe_load(file)
 
+        self.images = self._parse_images(self.data['regions'])
+
+    def _crop_image(self, img: np.ndarray, region: ROI):
+        crop = img[
+            region.y : region.y + region.h,
+            region.x : region.x + region.w,
+        ]
+        return crop
+
+    def _parse_images(self, dims: dict[str, dict[str, int]]):
+        img_items = {}
+        for key, dim in dims.items():
+            region = ROI(
+                dim['x'],
+                dim['y'],
+                dim['w'],
+                dim['h'],
+            )
+            img_items[key] = self._crop_image(self.img, region)
+        return img_items
+
+    def plot_images(self):
+        num_images = len(self.images) # + 1 # add one for full image
+        img_cols = 4
+        img_rows = num_images // 4 + int(num_images % 4 > 0)
+        fig, axes = plt.subplots(img_rows, img_cols)
+        for (label, img), ax in zip(self.images.items(), axes.flat):
+            ax.imshow(img)
+        
+        plt.tight_layout()
+        plt.show()
 
 if __name__ == '__main__':
-    img_path = pathlib.Path('../data/IMG_1470.PNG')
+    PROJECT_DIR = pathlib.Path(__file__).resolve().parents[1]
+    img_path = PROJECT_DIR / 'data' / 'IMG_1470.PNG'
     img = cv2.imread(img_path)
-    config_path = pathlib.Path('../config/ui.yaml')
-    print('--------------')
-    print(config_path)
+    config_path = PACKAGE_DIR / 'config' / 'ui.yaml'
     screen = Screen(img, config_path)
+    screen.plot_images()
---
+++ king_tensor/utils/help.py
@@ -0,0 +1,4 @@
+
+def main():
+    print('boxer [img_path]: interactively draw a box on img and get the resulting x, y, w, h')
+
