b197f2a19aef109227763cf4cf2ba8909f07c161
Eaglehawkinator <acoates@ucdavis.edu>
2025-12-01 16:11:40 -0600

    refactor paths to be in central location

---
+++ king_tensor/globals.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+PROJECT_DIR = Path(__file__).resolve().parents[1]
+PACKAGE_DIR = PROJECT_DIR / 'king_tensor'
+DATA_DIR = PROJECT_DIR / 'data'
+CONFIG_DIR = PACKAGE_DIR / 'config'
